016840e158cfa267159643288065c191eefe5a07
BugHunter-X <askzenai@gmail.com>
2025-02-08 03:02:04 +0100

    Add firebaseBug.js file

---
+++ firebaseBug.js
@@ -0,0 +1,20 @@
+const unsubscribe = onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/auth/web/user-properties
+    console.log(user);
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+// unsubscribe from the listener when the component unmounts to prevent memory leak
+
+// this part is missing in many examples and tutorials
+useEffect(() => {
+  return () => {
+    unsubscribe();
+  };
+}, []);
